bda889950741fd9db3314a5128bc350c0c2ff61a
Handsomeboy88888 <s4028673@rmit.edu.vn>
2024-09-07 17:39:06 +0700

    Add files via upload

---
+++ AI_Project.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer  
+from sklearn.preprocessing import OneHotEncoder      
+from sklearn.model_selection import KFold   
+from statistics import mean
+import joblib 
+
+raw_data = pd.read_csv(r'D:\pokemon_combined.csv')
+
+
+
+
+
